653bc9f3bee5d787037ec29e14f3331dff99e8a9
karthick37 <40197179+karthick37@users.noreply.github.com>
2020-02-29 05:09:03 +0530

    commit 2

--- getEmail.py
+++ getEmail.py
@@ -6,27 +6,82 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import mysql.connector
 from mysql.connector import Error
+import base64
+import json
+import pymysql
 
-def insertRows(msgId,lables,to,from_,subject,snippet,datetime):
-    print(msgId)
+#Connect to mySQL
+with open('mySQL_creds.json') as f:
+    sqlCreds = json.load(f)
+    mySqlUser = sqlCreds['mySqlAdmin']['user']
+    mySqlPwd = sqlCreds['mySqlAdmin']['pwd']
+    mySqlHost = sqlCreds['mySqlAdmin']['host']
+    mySqlHostIp = sqlCreds['mySqlAdmin']['hostIP']
+    mySqlDB = sqlCreds['mySqlAdmin']['db']
+
+connection = mysql.connector.connect(user=mySqlUser, password=mySqlPwd,
+                                    host=mySqlHost,
+                                    database=mySqlDB)
+def insertRows(data):
+    # take creds from json 
+    
+    insertQuery = "INSERT INTO tbl_mail (`mail_id`, `mail_lables`, `mail_snippet`, `mail_to`, `mail_from`,`mail_datetime`,`mail_subject`) "\
+                                   " VALUES (%s, %s, %s, %s, %s, %s, %s) "
+    truncate = "TRUNCATE tbl_mail"
     try:
-        connection = mysql.connector.connect(user='root', password='',
-                                    host='127.0.0.1',
-                                    database='db_gmail')
+        global connection
         cursor = connection.cursor()
-        insertQuery = """INSERT INTO tbl_mail (mail_id, mail_lables, mail_to, mail_from,mail_subject,mail_snippet,mail_datetime) 
-                                    VALUES ('msgId',lables,to,from_,subject,snippet,datetime) """
-        connection.commit()
+
+        #Truncate everytime to avoid MessageID conflict
+        cursor.execute(truncate)
+        cursor.executemany(insertQuery,data)
         print(insertQuery)
+        connection.commit()
     except mysql.connector.Error as error:
         print("Failed to insert into MySQL table {}".format(error))
     finally:
         if (connection.is_connected()):
             cursor.close()
             connection.close()
+        else:
+            print('not connected')
  
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+def returnVal(headers,param):
+    for header in headers:
+        if header['name'] == param:
+            return str(header['value'])
+            break
+
+def getMsgID(header,filter,value):
+
+    #mysql connector having some issues with Select Queries. Using a diff package for select querirs
+    conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='', db='db_gmail')
+    cur = conn.cursor()
+
+    if filter == "contains":
+        searchQuery = "SELECT mail_id FROM tbl_mail WHERE mail_from LIKE %s"
+        cur.execute(searchQuery,"%"+value+"%")
+    if filter == "does not contains":
+        searchQuery = "SELECT mail_id FROM tbl_mail WHERE mail_from NOT LIKE %s"
+        cur.execute(searchQuery,"%"+value+"%")
+    if filter == "equals":
+        searchQuery = "SELECT mail_id FROM tbl_mail WHERE mail_from = %s"
+        cur.execute(searchQuery,value)
+    if filter == "does not equals":
+        searchQuery = "SELECT mail_id FROM tbl_mail WHERE %s != %s"
+        cur.execute(searchQuery,value)
+    
+    ids = []
+    for row in cur:
+        ids.append(row[0])
+
+    cur.close()
+    conn.close()
+
+    return ids
 
 def main():
     creds = None
@@ -56,42 +111,75 @@ def main():
     results = service.users().messages().list(userId='me',labelIds=['INBOX'],q="is:unread",maxResults=msgCount).execute()
     messages = results.get('messages', [])
     
+    mailList = []
+
     if not messages:
         print('No mails found')
     else:
         i = 1
         for message in messages:
+            mail = []
             #Get Message Details
             msg = service.users().messages().get(userId='me', id=message['id']).execute()
+
             #fetching values for DB
-            print(msg)
-            lables = msg['labelIds']
-            msgId = msg['id']
+            msgId = str(msg['id'])
+            mail.append(msgId)
+
+            lables = ','.join(msg['labelIds'])
+            mail.append(lables)
+            
             headers = msg['payload']['headers']
-            snippet = msg['snippet']
-            for header in headers:
-                if header['name'] == "Delivered-To":
-                    toMail = header['value']
-                else:
-                    toMail = ""
-                if header['name'] ==  "From":
-                    fromMail = header['value']
-                    print(fromMail)
-                else:
-                    fromMail = ""
-                if header['name'] == "Subject":
-                    subjectMail = header['value']
-                else:
-                    subjectMail = ""
-                if header['name'] == "Date":
-                    dateMail = header = header['value']
-                else:
-                    dateMail = ""
+            
+            snippet = str(msg['snippet'])
+            mail.append(snippet)
+            
+            toMail = returnVal(headers,"Delivered-To")
+            mail.append(toMail)
 
-            #insertRows
-            if(insertRows(msgId,lables,toMail,fromMail,subjectMail,snippet,dateMail)):
-                print(i,' row inserted')
+            fromMail = returnVal(headers,"From")
+            mail.append(fromMail)
+            
+            Date = returnVal(headers,"Date")
+            mail.append(Date)
 
+            subjectMail = returnVal(headers,"Subject")
+            mail.append(subjectMail)
+
+            # body = msg['body']['data']
+            # decodedmsg = base64.b64decode(body)
+            # rawMsgStr = str(decodedmsg, "utf-8")
+
+            print(mail)
+            #append mail
+            mailList.append(mail)
+
+        #insertRows
+        # mailList = tuple(i for i in mailList)
+        insertRows(mailList)
+
+    def criteriaAction(action,messageIds):
+        for messageId in messageIds:
+            print(messageIds)
+
+    #Play with sql data
+    #import rules
+    with open('rules.json') as ruleFile:
+        rules = json.load(ruleFile)
+
+        for rule in rules.values():
+            if rule['predicate'] == "All":
+                action = rule['action']
+                for criteria in rule['criteria']:                    
+                    header = criteria['header']
+                    value = criteria['value']
+                    filter = criteria['filter']
+                    messageIds = getMsgID(header,filter,value)
+                    
+                    if messageIds != '':
+                        TakeAction = criteriaAction(action,messageIds)
+                    else:
+                        print("No Id Found for this criteria")    
 
 
 if __name__ == '__main__':
